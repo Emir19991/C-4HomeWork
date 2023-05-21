2d95bf31fe870565a85bd7a3e9299b19f7183ade
Emir Akhmadeshin <akhmadeshin@list.ru>
2023-05-21 18:32:02 +0500

    homework4

---
+++ task_25/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+Console.WriteLine("Введите любое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите положительное число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int result = NaturalStep(numberA, numberB);
+Console.WriteLine($"{numberA}, {numberB} -> {result}");
+// Console.WriteLine("Ответ: " + result);
+
+
+int NaturalStep(int numA, int numB)
+{
+    int count = 1;
+    for (int i = 1; i <= numB; i++)
+    {
+        count = count * numA;
+    }
+    return count;
+}    
---
+++ task_27/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+// int Prompt(string message)
+// {
+//     System.Console.Write(message);
+//     string readInput = System.Console.ReadLine();
+//     int result = int.Parse(readInput);
+//     return result;
+// }
+
+// int SumAllDigit(int number)
+// {
+//     int result = 0;
+//     while (number > 0)
+//     {
+//         result += number % 10;
+//         number = number / 10;
+//     }
+//     return result;
+// }
+
+// int number = Prompt("Введите число ");
+// System.Console.WriteLine($"Сумма всех чисел в цифре {number} = {SumAllDigit(number)}");
+
+Console.Write("Введите число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+  int SumNumber(int numberN){
+    
+    int counter = Convert.ToString(numberN).Length;
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++){
+      advance = numberN - numberN % 10;
+      result = result + (numberN - advance);
+      numberN = numberN / 10;
+    }
+   return result;
+  }
+
+int sumNumber = SumNumber(numberN);
+Console.WriteLine("Сумма цифр в числе: " + sumNumber);
